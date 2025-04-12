e382ee8845a6333831ff3ebb7e2b819a12cd1e49
pyckcel <112999580+pyckcel@users.noreply.github.com>
2025-04-12 19:43:50 -0300

    Create skywars mine aura.lua

---
+++ skywars mine aura.lua
@@ -0,0 +1,29 @@
+-- Mine Aura v1 (Undermine Aura v1)
+-- made by hash_lol
+
+-- Controlador global
+getgenv().script = true
+
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+
+local detectionRange = 25 -- detection range
+
+while getgenv().script do
+    wait(0.1)
+    for _, item in pairs(workspace:GetDescendants()) do
+        if item:IsA("Part") and item.Name == "Block" then
+            local distance = (humanoidRootPart.Position - item.Position).magnitude
+            if distance <= detectionRange then
+                local axe = player.Backpack:FindFirstChild("Axe")
+                if axe then
+                    local remoteEvent = axe:FindFirstChild("RemoteEvent")
+                    if remoteEvent then
+                        remoteEvent:FireServer(item)
+                    end
+                end
+            end
+        end
+    end
+end
